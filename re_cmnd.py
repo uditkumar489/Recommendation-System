85b185ce944d1f78c5b8d2a75fd68b20e5562b18
udit kumar <uditkumar489@gmail.com>
2018-01-13 07:49:01 +0530

    essential libraries imported
    ..

---
+++ re_cmnd.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 13 07:44:25 2018
+
+@author: Udit
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
