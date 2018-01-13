f726830e2547220318d18b2ee899175253ea85e8
udit kumar <uditkumar489@gmail.com>
2018-01-13 07:49:48 +0530

    data preProcessed
    done..

--- re_cmnd.py
+++ re_cmnd.py
@@ -9,3 +9,9 @@ Created on Sat Jan 13 07:44:25 2018
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Data Preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
